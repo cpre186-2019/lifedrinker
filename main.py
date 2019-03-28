a6fec9ea81237f66e52f4019e498757245c6a5ff
Alexis Renderos <renderosalexis17@gmail.com>
2019-03-27 20:57:34 -0500

    Updated main, and mainsetup, firmata now works! -Alexis

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 # Written by: Alexis Renderos
 
 from pyfirmata import Arduino, util # used to talk to Arduino over USB.
-import cv2 # openCV library
+# import cv2 # openCV library
 import functions # local functions, syntax is functions.(method)(args)
 import mainsetup # setup function, runs once.
 import mainloop # loop-dee-loop
-import vision # our vision code!
+# import vision # our vision code!
 
 print(functions.map(10, 0, 100, 0, 5000))
-# mainsetup.setup()
+mainsetup.setup()
 # mainloop.mainLoop()
--- mainsetup.py
+++ mainsetup.py
@@ -1,15 +1,17 @@
+from pyfirmata import Arduino, util
+
 def setup():
     # setup runs once
-    board = Arduino('/dev/tty.usbserial-A6008rIF')
+    board = Arduino('/dev/ttyACM0')
     board.digital[13].write(1) # sets the onboard LED to ON.
 
     analog_0 = board.get_pin('a:0:i') # pot pin
-    pin0 = board.get_pin('d:0:o') # pin0 for motor 1
-    pin1 = board.get_pin('d:1:o') # pin1 for motor 1
-    pin2 = board.get_pin('d:2:o') # pin2 for motor 2
-    pin3 = board.get_pin('d:3:o') # pin3 for motor 2
-    pwm1 = board.get_pin('d:5:p') # pwm pin for motor 1
-    pwm2 = board.get_pin('d:6:p') # pwm pin for motor 2
+    pin0 = board.get_pin('d:8:o')  # pin0 for motor 1
+    pin1 = board.get_pin('d:9:o')  # pin1 for motor 1
+    pin2 = board.get_pin('d:10:o') # pin2 for motor 2
+    pin3 = board.get_pin('d:11:o') # pin3 for motor 2
+    pwm1 = board.get_pin('d:5:p')  # pwm pin for motor 1
+    pwm2 = board.get_pin('d:6:p')  # pwm pin for motor 2
 
     # start these values with the correct values for a forward shot.
     pin0state = 0
