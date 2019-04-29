a866b9a2b1d9aba5d1acf98faefd313a4b0ef112
Alexis Renderos <renderosalexis17@gmail.com>
2019-04-28 19:13:24 -0500

    Okay, things are a lot less on fire now, poster should be done? Theo af tho lmao -Alexis

--- mainLiveView.py
+++ mainLiveView.py
@@ -7,7 +7,7 @@ import cv2
 
 # IMPORTANT FOR DEBUG PURPOSES
 # Pretty obvs, but make this true to have debug output
-debug = True
+debug = False
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
